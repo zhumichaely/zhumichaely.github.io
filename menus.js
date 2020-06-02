0dc1a2020bad2423f17ea1e300391250d5a058a5
zhumichaely <57302029+zhumichaely@users.noreply.github.com>
2020-06-01 21:40:28 -0400

    nav menu now shows on click rather than hover

--- menus.js
+++ menus.js
@@ -1,3 +1,12 @@
-function toggleUpDown {
-    
-}
+const menuButton = document.getElementById("navbutton");
+menuButton.addEventListener("click", () => {
+  const menu = document.getElementById("navcontent"),
+    image = menuButton.children[0];
+  if (menu.style.display === "none") {
+    image.setAttribute("src", "images/upArrow.png");
+    menu.style.display = "block";
+  } else {
+    image.setAttribute("src", "images/downArrow.png");
+    menu.style.display = "none";
+  }
+});
