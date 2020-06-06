dac59d33cd0dfd1b1fd764cec59d5c26cd17abac
zhumichaely <57302029+zhumichaely@users.noreply.github.com>
2020-06-06 17:54:44 -0400

    slightly broken animations

--- menus.js
+++ menus.js
@@ -1,13 +1,45 @@
 const menuButton = document.getElementById("navbutton");
 const menu = document.getElementById("navcontent");
-menu.style.display = "none";
+const links = document.getElementById("links");
+const image = menuButton.children[0];
+let anim;
+
 menuButton.addEventListener("click", () => {
-  const image = menuButton.children[0];
-  if (menu.style.display === "none") {
-    image.setAttribute("src", "images/upArrow.png");
-    menu.style.display = "block";
+  if (getComputedStyle(menu).display === "none") {
+    anim = requestAnimationFrame(menuDown);
   } else {
-    image.setAttribute("src", "images/downArrow.png");
-    menu.style.display = "none";
+    links.style.top = links.scrollHeight;
+    anim = requestAnimationFrame(menuUp);
   }
 });
+
+function menuDown() {
+  const pos = parseFloat(getComputedStyle(links).top);
+  const end = links.scrollHeight;
+  if (pos < end) {
+    links.style.top = (pos + 20) + "px";
+    anim = requestAnimationFrame(menuDown);
+  }
+  else {
+    links.style.top = "100%";
+    image.setAttribute("src", "images/upArrow.png");
+    menu.style.display = "block";
+    cancelAnimationFrame(anim);
+  }
+}
+
+function menuUp() {
+  const pos = parseFloat(getComputedStyle(links).top);
+  const end = menu.scrollHeight / 3;
+  console.log(pos);
+  if (pos > end) {
+    links.style.top = (pos - 20) + "px";
+    anim = requestAnimationFrame(menuUp);
+  }
+  else {
+    menu.style.display = "none";
+    links.style.top = "100%";
+    image.setAttribute("src", "images/downArrow.png");
+    cancelAnimationFrame(anim);
+  }
+}
